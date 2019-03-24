8dedb40dc80b8badee37171f889035c05d2b08a8
Jose Herrera <josehr1108@gmail.com>
2019-03-24 01:26:43 -0600

    server.js para servir el app minificado

--- src/App.js
+++ src/App.js
@@ -1,28 +1,45 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import _ from 'lodash';
+import React, {Component} from 'react';
+import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+
+const API_KEY = 'AIzaSyC3Mj8alXdYjL-Nqf_3cieG27X1q7SCD08';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+    constructor(props){
+        super(props);
+
+        this.state = { videos: [] , selectedVideo: null };
+        this.videoSearch("cocofunka");
+    }
+
+    videoSearch(term){
+        YTSearch({key: API_KEY,term: term},(videos) => {
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0]
+            });
+        });
+    }
+
+    render = () => {
+        const videoSearch = _.debounce(term => { //delay de 300ms para volver a invocar la función una vez llamada
+           this.videoSearch(term)
+        },300);
+
+        return (
+            <div>
+                <SearchBar onSearchTermChange={videoSearch}/>
+                <VideoDetail video={this.state.selectedVideo}/>
+                <VideoList
+                    videos={this.state.videos}
+                    onVideoSelect={selectedVideo => this.setState({selectedVideo})}/>
+            </div>
+        )
+    }
+};
 
 export default App;
